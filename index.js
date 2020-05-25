4e33a4680f011f470e287fdfef94b6b1b2c05c22
AB3000 <aarushban@gmail.com>
2020-05-24 21:42:46 -0700

    Connection to database working

--- index.js
+++ index.js
@@ -2,12 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// IMPORT MODELS
+require('./models/Product');
+
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+//aSK2gJ8ICMGOI8pL
 
+mongoose.Promise = global.Promise;
+mongoose.connect(
+    "mongodb+srv://test:aSK2gJ8ICMGOI8pL@cluster0-hetxl.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function(error) {
+      if (error) {
+        console.log("Couldn't connect to database");
+      } else {
+        console.log("Connected To Database");
+      }
+    }
+  );
+  
 app.use(bodyParser.json());
+
+//IMPORT ROUTES
+require('./routes/productRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
